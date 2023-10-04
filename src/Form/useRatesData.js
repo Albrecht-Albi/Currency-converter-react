e1f42d0260754256f30599dfc0b030a305cefb69
Albrecht-Albi <albrecht.bar1@gmail.com>
2023-10-04 10:41:27 +0200

    update error status

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -6,12 +6,15 @@ export const useRatesData = () => {
         status: "loading",
     });
 
-    const url = "https://api.exchangerate.host/latest?base=pln";
+    const url = "http://api.currencyapi.com/v3/latest?apikey=cur_live_cotoGa9patQrenX9NUprLYP7HxRhcHV09dUHTlHZ&currencies=&base_currency=PLN"
 
     useEffect(() => {
         const fetchRates = async () => {
             try {
                 const response = await axios.get(url);
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
 
                 setRatesData({
                     status: "success",
